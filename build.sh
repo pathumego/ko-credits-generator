33dc6210494716ba5cb7e83471ed46cc5975cf9b
Pathum Egodawatta <pathumego@gmail.com>
2024-01-03 11:57:41 +0530

    Adding S03E06 content

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 # asciidoctor, weasyprint, imagemagik
 
 
-# asciidoctor episode-titles.adoc -o episode-titles.html ;
-# weasyprint episode-titles.html episode-titles.pdf ;
-asciidoctor credits_s03e03.adoc -o credits_s03e03.html ;
-weasyprint credits_s03e03.html credits_s03e03.pdf ;
+asciidoctor episode-titles.adoc -o episode-titles.html ;
+weasyprint episode-titles.html episode-titles.pdf ;
+# asciidoctor credits_s03e03.adoc -o credits_s03e03.html ;
+# weasyprint credits_s03e03.html credits_s03e03.pdf ;
 
-asciidoctor credits_s03e04.adoc -o credits_s03e04.html ;
-weasyprint credits_s03e04.html credits_s03e04.pdf ;
+asciidoctor credits_s03e06.adoc -o credits_s03e06.html ;
+weasyprint credits_s03e06.html credits_s03e06.pdf ;
 
-convert -density 400 credits_s03e04.pdf OUT/s03e04/s03e04.png ;
+convert -density 400 credits_s03e06.pdf OUT/s03e06/s03e06.png ;
 
 
 # cleanup
